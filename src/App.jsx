e8891b089a197c78532eacbf1f36e919269f7624
KulkarniSuraj <30728951+KulkarniSuraj@users.noreply.github.com>
2023-10-02 15:24:31 +0530

    Creates basic form template

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,69 @@
-import { Link, Container } from "@mui/material";
+import { Button, Box, Container, TextField } from "@mui/material";
 import Section from "./components/Section";
 import Banner from "./components/Banner";
 
 function App() {
   return (
-    <main>
+    <main
+      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+    >
       <Container
         sx={{ border: "1px solid black", marginTop: "20px" }}
         maxWidth="sm"
       >
         <Banner />
-        <Section question={"Planned Downtime: Vizportal Production Environment"}>
-        <strong>Activity Description: </strong>
-        Unavailability of Vizportal application due to patching activity of server NMVAZ-03 <br />
-        <strong>Planned Downtime date and time: </strong>
-        Friday 24th December 12:00 PM IST to Friday 24th December 4:00 PM IST
-          </Section>
-          <Section question={"How does this affect me?"}>
-        Viz Portal <Link href="https://vizport.axolo.com">https://vizport.axolo.com</Link> will be inaccessible and unavailable during downtime.
-          </Section>
-          <Section question={"Who is impacted?"}>
-        Employees and contractors with access to Vizportal production.
-        Please cascade this communication to all stakeholders.
-          </Section>
-          <Section question={"Questions and Concerns"}>
-        Please do not reply to this email. In case of any questions or concerns, please drop an email to <Link href="mailto:vizsupport@axolo.com">vizsupport@axolo.com</Link>
-          </Section>
+        <Section
+          question={"Planned Downtime: Vizportal Production Environment"}
+        >
+          <strong>Activity Description: </strong>
+          <TextField
+            id="standard-multiline-static"
+            multiline
+            variant="standard"
+            defaultValue="Unavailability of Vizportal application due to patching activity of server NMVAZ-03"
+            fullWidth
+          />
+          <strong>Planned Downtime date and time: </strong>
+
+          <TextField
+            id="standard-multiline-static"
+            multiline
+            variant="standard"
+            defaultValue="Friday 24th December 12:00 PM IST to Friday 24th December 4:00 PM IST"
+            fullWidth
+          />
+        </Section>
+        <Section question={"How does this affect me?"}>
+          <TextField
+            id="standard-multiline-static"
+            multiline
+            variant="standard"
+            defaultValue="Viz Portal https://vizport.axolo.com will be inaccessible and unavailable during downtime."
+            fullWidth
+          />
+        </Section>
+        <Section question={"Who is impacted?"}>
+          <TextField
+            id="standard-multiline-static"
+            multiline
+            variant="standard"
+            defaultValue="Employees and contractors with access to Vizportal production. Please cascade this communication to all stakeholders."
+            fullWidth
+          />
+        </Section>
+        <Section question={"Questions and Concerns"}>
+          <TextField
+            id="standard-multiline-static"
+            multiline
+            variant="standard"
+            defaultValue="Please do not reply to this email. In case of any questions or concerns, please drop an email to vizsupport@axolo.com"
+            fullWidth
+          />
+        </Section>
       </Container>
+      <Box sx={{ m: 2, display: "flex", justifyContent: "center" }}>
+        <Button variant="contained">Submit</Button>
+      </Box>
     </main>
   );
 }
