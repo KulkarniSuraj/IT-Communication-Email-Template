6e252c6c7e02f1d7e7ea6eb6aa3a9c84e4afefbb
KulkarniSuraj <30728951+KulkarniSuraj@users.noreply.github.com>
2023-10-11 21:32:37 +0530

    adds preview functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Button, Box, Container, TextField } from "@mui/material";
 import Section from "./components/Section";
+import Answer from "./components/Answer";
 import Banner from "./components/Banner";
 import { useState } from "react";
 import DATA from "./data";
@@ -23,54 +24,74 @@ function App() {
           <Banner />
           <Section question={DATA.downtime_heading.Q}>
             <strong>{DATA.downtime_heading.Q_desc}</strong>
-            <TextField
-              id="description"
-              multiline
-              variant="standard"
-              defaultValue={DATA.downtime_heading.A_desc}
-              fullWidth
-            />
-            <strong>{DATA.downtime_heading.Q_time}</strong>
+            {showPreview ? (
+              <Answer text={DATA.downtime_heading.A_desc} />
+            ) : (
+              <TextField
+                id="description"
+                multiline
+                variant="standard"
+                defaultValue={DATA.downtime_heading.A_desc}
+                fullWidth
+              />
+            )}
 
-            <TextField
-              id="time"
-              multiline
-              variant="standard"
-              defaultValue={DATA.downtime_heading.A_time}
-              fullWidth
-            />
+            <strong>{DATA.downtime_heading.Q_time}</strong>
+            {showPreview ? (
+              <Answer text={DATA.downtime_heading.A_time} />
+            ) : (
+              <TextField
+                id="time"
+                multiline
+                variant="standard"
+                defaultValue={DATA.downtime_heading.A_time}
+                fullWidth
+              />
+            )}
           </Section>
           <Section question={DATA.effect.Q}>
-            <TextField
-              id="effect"
-              multiline
-              variant="standard"
-              defaultValue={DATA.effect.A}
-              fullWidth
-            />
+            {showPreview ? (
+              <Answer text={DATA.effect.A} />
+            ) : (
+              <TextField
+                id="effect"
+                multiline
+                variant="standard"
+                defaultValue={DATA.effect.A}
+                fullWidth
+              />
+            )}
           </Section>
           <Section question={DATA.impact.Q}>
-            <TextField
-              id="impact"
-              multiline
-              variant="standard"
-              defaultValue={DATA.impact.A}
-              fullWidth
-            />
+            {showPreview ? (
+              <Answer text={DATA.impact.A} />
+            ) : (
+              <TextField
+                id="impact"
+                multiline
+                variant="standard"
+                defaultValue={DATA.impact.A}
+                fullWidth
+              />
+            )}
           </Section>
           <Section question={DATA.contact.Q}>
-            <TextField
-              id="contact"
-              multiline
-              variant="standard"
-              defaultValue={DATA.contact.A}
-              fullWidth
-            />
+            {showPreview ? (
+              <Answer text={DATA.contact.A} />
+            ) : (
+              <TextField
+                id="contact"
+                multiline
+                variant="standard"
+                defaultValue={DATA.contact.A}
+                fullWidth
+              />
+            )}
           </Section>
         </Container>
         <Box sx={{ m: 2, display: "flex", justifyContent: "center" }}>
           <Button variant="outlined" sx={{ mx: 2 }} onClick={handlePreview}>
-            Preview
+            {showPreview ? "Edit" : "Preview"}
           </Button>
           <Button variant="contained" type="submit">
             download
