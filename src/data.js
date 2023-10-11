fb49c75b2a52fb4709d7a9ad17f4da0598e10edc
KulkarniSuraj <30728951+KulkarniSuraj@users.noreply.github.com>
2023-10-11 21:18:36 +0530

    reduced nesting of object

--- src/data.js
+++ src/data.js
@@ -1,25 +1,26 @@
-const DATA = [
-  {
-    "Planned Downtime: Vizportal Production Environment": {
-      "Activity Description:":
-        "Unavailability of Vizportal application due to patching activity of server NMVAZ-03",
-      "Planned Downtime date and time:":
-        "Friday 24th December 12:00 PM IST to Friday 24th December 4:00 PM IST",
-    },
+const DATA = {
+  downtime_heading: {
+    Q: "Planned Downtime: Vizportal Production Environment",
+    Q_desc: "Activity Description:",
+    A_desc:
+      "Unavailability of Vizportal application due to patching activity of server NMVAZ-03",
+    Q_time: "Planned Downtime date and time:",
+    A_time:
+      "Friday 24th December 12:00 PM IST to Friday 24th December 4:00 PM IST",
   },
 
-  {
-    "How does this affect me?":
-      "Viz Portal https://vizport.axolo.com will be inaccessible and unavailable during downtime",
+  effect: {
+    Q: "How does this affect me?",
+    A: "Viz Portal https://vizport.axolo.com will be inaccessible and unavailable during downtime",
   },
-  {
-    "Who is impacted?":
-      "Employees and contractors with access to Vizportal production. Please cascade this communication to all stakeholders.",
+  impact: {
+    Q: "Who is impacted?",
+    A: "Employees and contractors with access to Vizportal production. Please cascade this communication to all stakeholders.",
   },
-  {
-    "Questions and Concerns":
-      "Please do not reply to this email. In case of any questions or concerns, please drop an email to vizsupport@axolo.com",
+  contact: {
+    Q: "Questions and Concerns",
+    A: "Please do not reply to this email. In case of any questions or concerns, please drop an email to vizsupport@axolo.com",
   },
-];
+};
 
 export default DATA;
